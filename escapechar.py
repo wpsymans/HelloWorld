c62ca6d4b90385840ecd77b75b6c628cc49766ec
William Symans <wsymans@j2interactive.com>
2021-01-16 15:03:17 -0800

    Initial commit

---
+++ escapechar.py
@@ -0,0 +1,9 @@
+splitString = "This string has been \nsplit over \nseveral \nlines"
+print(splitString)
+
+tabbedString = "1 \t2 \t3 \t4 \t5"
+print(tabbedString)
+
+print('the pet shop owner said "no, no \'e\'s uh\'s hresting"')
+
+print(''' "testing a trip quote " ''')
---
+++ strings.py
@@ -0,0 +1,10 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('we can even include "quotes" in strings')
+print("hello" + " World")
+
+greeting = "Hello"
+name = input("Please enter your name ")
+
+print(greeting + ' ' + name)
