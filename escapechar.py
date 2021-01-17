31a437a6e9fb992ce69175b6a597bb75e02c80a8
William Symans <wsymans@j2interactive.com>
2021-01-17 06:54:16 -0800

    Commiting escapechar

--- escapechar.py
+++ escapechar.py
@@ -7,3 +7,12 @@ print(tabbedString)
 print('the pet shop owner said "no, no \'e\'s uh\'s hresting"')
 
 print(''' "testing a trip quote " ''')
+
+print(""" heres a string broken \
+over several lines""")
+
+#print("C:\users\symans\notes.txt")
+
+print("C:\\users\\symans\\notes.txt")
+
+print(r"C:\users\symans\notes.txt")
